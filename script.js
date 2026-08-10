9edd3d9ae278a781c0501b042712aa87aa4dedc1
b3434474-art <b3434474@gmail.com>
2026-08-10 00:02:49 -0500

    Update script.js

--- script.js
+++ script.js
@@ -25,9 +25,30 @@ document.getElementById("favorites-button").addEventListener("click", function()
 });
 
 const channels = document.querySelectorAll(".channel-card");
+const categories = document.querySelectorAll(".category");
 
 channels.forEach(channel => {
   channel.addEventListener("click", function() {
     window.open(this.dataset.url, "_blank");
+  });
+});
+
+categories.forEach(category => {
+  category.addEventListener("click", function() {
+    categories.forEach(button => {
+      button.classList.remove("active");
+    });
+
+    this.classList.add("active");
+
+    const selectedCategory = this.dataset.category;
+
+    channels.forEach(channel => {
+      if (selectedCategory === "all" || channel.dataset.category === selectedCategory) {
+        channel.style.display = "flex";
+      } else {
+        channel.style.display = "none";
+      }
+    });
   });
 });
